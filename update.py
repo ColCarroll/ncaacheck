a39ddb552f8c00af3efc085784846a22fc183c5e
Colin Carroll <colinc@colinc-mbp2.fios-router.home>
2015-03-20 01:19:54 -0400

    Day 1 results updated

--- update.py
+++ update.py
@@ -1,6 +1,5 @@
-#!/Users/colinc/anaconda/bin/python
-import pandas
 import os
+import numpy
 import scipy
 import csv
 
@@ -13,45 +12,55 @@ def get_preds():
     return os.listdir(PRED_DIR)
 
 
-def log_loss(act, pred):
+def log_loss(actual, predicted):
     epsilon = 1e-15
-    pred = scipy.maximum(epsilon, pred)
-    pred = scipy.minimum(1 - epsilon, pred)
-    ll = sum(act * scipy.log(pred) + scipy.subtract(1, act) * scipy.log(scipy.subtract(1, pred)))
-    return ll * -1.0 / len(act)
+    actual = numpy.array(actual)
+    predicted = scipy.minimum(1 - epsilon, scipy.maximum(epsilon, numpy.array(predicted)))
+    return -(actual * scipy.log(predicted) + (1 - actual) * scipy.log(1 - predicted)).mean()
+
+
+def get_results(filename):
+    with open(filename, 'rU') as buff:
+        results = {row["id"]: float(row["pred"]) for row in csv.DictReader(buff)}
+    return results
 
 
 def score_all():
-    result_df = pandas.read_csv(os.path.join(DATA_DIR, 'results.csv'))
-
-    loss_lookup = []
+    results = get_results(os.path.join(DATA_DIR, "results.csv"))
+    losses = []
+    game_ids, actual = zip(*results.items())
     for filename in get_preds():
-        df = pandas.read_csv(os.path.join(PRED_DIR, filename))
+        file_preds = get_results(os.path.join(PRED_DIR, filename))
+        predictions = [file_preds[game_id] for game_id in game_ids]
+        losses.append((filename, log_loss(actual, predictions)))
+    return sorted(losses, key=lambda j: j[1])
 
-        p, r = [], []
 
-        for index, row in result_df.iterrows():
-            for index2, row2 in df.loc[df['id'] == row['id']].iterrows():
-                if row['id'] != row2['id']:
-                    continue
-                else:
-                    p.append(row2['pred'])
-                    r.append(row['result'])
-                    break
+def team_lookup():
+    with open(os.path.join(DATA_DIR, "teams.csv")) as buff:
+        return {row["team_id"]: row["team_name"] for row in csv.DictReader(buff)}
 
-        loss_lookup.append((filename, log_loss(r, p)))
 
-    return sorted(loss_lookup, key=lambda j: j[1])
+def print_results():
+    teams = team_lookup()
+    with open(os.path.join(DATA_DIR, "results.csv")) as buff:
+        next(buff)
+        for j, row in enumerate(buff, start=1):
+            game_id, result = row.strip().split(",")
+            _, team_one, team_two = game_id.split("_")
+            result = int(result)
+            outcome = "beat" if result == 1 else "lost to"
+            print("{:d}. {:s} {:s} {:s}".format(j, teams[team_one], outcome, teams[team_two]))
 
 
 def write_scores():
     with open(os.path.join(DIR, 'readme.md'), 'wb') as buff:
-        writer = csv.writer(buff, quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(['index|filename|score'])
-        writer.writerow(['-----|-----|-----'])
-        for j, row in enumerate(score_all(), start=1):
-            writer.writerow([str(j) + '|' + str(row[0]) + '|' + str(row[1])])
+        buff.write('index|filename|score\n')
+        buff.write('-----|-----|-----\n')
+        buff.write("\n".join(
+            ["{:s}".format("|".join(map(str, [j, row[0], row[1]]))) for j, row in enumerate(score_all(), start=1)]))
 
 
 if __name__ == '__main__':
+    print_results()
     write_scores()
