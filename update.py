4820fd0e18decd4b9785a3236d77b878c5c6471f
Colin Carroll <colinc@colinc-mbp2.fios-router.home>
2015-03-28 01:59:21 -0400

    Updated results, allow you to print out your individual outcomes

--- update.py
+++ update.py
@@ -2,6 +2,7 @@ import os
 import numpy
 import scipy
 import csv
+import itertools
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(DIR, "data")
@@ -23,6 +24,64 @@ def get_results(filename):
     with open(filename, 'rU') as buff:
         results = {row["id"]: float(row["pred"]) for row in csv.DictReader(buff)}
     return results
+
+
+def score_possible(ordered_team_list=map(str, [1112, 1458, 1246, 1323, 1181, 1211, 1257, 1277]), username="carroll"):
+    """
+    this is a little brittle:
+    ordered_team_list has to be strings of team ids, in order so that everything collapses the right way --
+    so if the winner of kentucky vs nd plays the winner of wisco vs az, then you can't put duke in the middle of those
+    four.
+    """
+    outcomes = []
+    teams = team_lookup()
+    games_left = len(ordered_team_list) - 1
+    with open(os.path.join(DATA_DIR, "results.csv"), 'rU') as buff:
+        results = [{"id": row["id"], "pred": float(row["pred"])} for row in csv.DictReader(buff)]
+    game_ids = [j["id"] for j in results] + ["" for _ in range(games_left)]
+    actual = [j["pred"] for j in results] + [0 for _ in range(games_left)]
+
+    predictions = {}
+    for filename in get_preds():
+        predictions[filename] = get_results(os.path.join(PRED_DIR, filename))
+
+    for possibility in itertools.product("01", repeat=games_left):
+        poss = list(map(int, possibility))
+        new_ids = []
+        wins = []
+        teams_left = ordered_team_list[:]
+        for idx, j in enumerate(poss):
+            team_one, team_two = teams_left[2 * idx: 2 * idx + 2]
+            if team_two < team_one:
+                j = (j + 1) % 2
+                team_one, team_two = team_two, team_one
+            if j == 0:
+                wteam, lteam = team_two, team_one
+            else:
+                wteam, lteam = team_one, team_two
+            teams_left.append(wteam)
+            new_ids.append("2015_{:s}_{:s}".format(team_one, team_two))
+            wins.append("{:s} def. {:s}".format(teams[wteam], teams[lteam], new_ids[-1], j))
+
+        game_ids[-games_left:] = new_ids
+        actual[-games_left:] = poss
+
+        losses = []
+        for filename, preds in predictions.iteritems():
+            losses.append((filename, log_loss(actual, [preds[game_id] for game_id in game_ids])))
+
+        team_places = {}
+        for j, row in enumerate(sorted(losses, key=lambda j: j[1]), start=1):
+            team_name = row[0].split("_")[0].replace("-", " ")
+            if team_name not in team_places:
+                team_places[team_name] = len(team_places) + 1
+            if username in team_name:
+                outcomes.append([team_places[team_name]] + wins)
+                break
+    with open('{:s}_outcomes.txt'.format(username), 'w') as buff:
+        for k in sorted(outcomes, key=lambda j: j[0]):
+            buff.write("{:d}.\n\t{:s}".format(k[0], "\n\t".join(k[1:])))
+            buff.write("\n")
 
 
 def score_all():
@@ -60,12 +119,13 @@ def write_scores():
         buff.write('bracket_place|team_place|filename|score\n')
         buff.write('-----|-----|-----|-----\n')
         for j, row in enumerate(score_all(), start=1):
-            team_name = row[0].split("_")[0].replace("-"," ")
+            team_name = row[0].split("_")[0].replace("-", " ")
             if team_name not in team_places:
                 team_places[team_name] = len(team_places) + 1
             buff.write("{:s}\n".format("|".join(map(str, [j, team_places[team_name], row[0], row[1]]))))
 
 
 if __name__ == '__main__':
-    print_results()
-    write_scores()
+    # print_results()
+    # write_scores()
+    score_possible()
